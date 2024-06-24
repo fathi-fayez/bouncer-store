5e16c4b7c2e8e273dda58c77cd46d053c9f6103c
fathi-fayez <fathifayez155@gmail.com>
2024-06-24 23:49:10 +0300

    fnish the primary design for all the project pages

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -7,9 +7,7 @@
     :modules="modules"
     class="mySwiper"
   >
-    <swiper-slide v-for="(photo, index) in photos" :key="index"
-      ><img :src="`src/assets/images/${photo}`" alt=""
-    /></swiper-slide>
+    <swiper-slide><img src="/src/assets/images/CAROUSEL.png" alt="" /></swiper-slide>
   </swiper>
 </template>
 <script setup>
@@ -24,5 +22,4 @@ import '../assets/main.css'
 import { Pagination } from 'swiper/modules'
 
 const modules = ref([Pagination])
-const photos = ref(['CAROUSEL.png', '107926695_175772.jpg', '107920287_198894.jpg'])
 </script>
