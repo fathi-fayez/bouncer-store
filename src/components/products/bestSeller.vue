3bee7a27735a3f96f527d9ddfc97dc58a03957f1
fathi-fayez <fathifayez155@gmail.com>
2024-06-27 13:07:36 +0300

    add hove effict for product elements

--- src/components/products/bestSeller.vue
+++ src/components/products/bestSeller.vue
@@ -5,10 +5,10 @@
       <div class="row">
         <!-- product card -->
         <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
-          <div class="card border-0 shadow-sm text-center">
+          <div class="card border-1 shadow-sm text-center">
             <img src="/src/assets/images/50061505_575246.png" alt="product.id" />
 
-            <div class="content">
+            <div class="card-body">
               <h5 class="my-5">Typesetting industry</h5>
               <div class="rating">
                 <i class="fa-solid fa-star" style="color: #ffc600"></i>
@@ -21,14 +21,18 @@
                 <span class="last-price">$499</span> <span class="old-price">$599</span>
               </div>
             </div>
+            <div class="overlay-buttons">
+              <button class="add-to-favorite"><i class="fa-regular fa-heart"></i></button>
+              <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"> </i></button>
+            </div>
           </div>
         </div>
         <!-- product card -->
         <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
-          <div class="card border-0 shadow-sm text-center">
+          <div class="card border-1 shadow-sm text-center">
             <img src="/src/assets/images/50061505_575246.png" alt="product.id" />
 
-            <div class="content">
+            <div class="card-body">
               <h5 class="my-5">Typesetting industry</h5>
               <div class="rating">
                 <i class="fa-solid fa-star" style="color: #ffc600"></i>
@@ -41,14 +45,18 @@
                 <span class="last-price">$499</span> <span class="old-price">$599</span>
               </div>
             </div>
+            <div class="overlay-buttons">
+              <button class="add-to-favorite"><i class="fa-regular fa-heart"></i></button>
+              <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"> </i></button>
+            </div>
           </div>
         </div>
         <!-- product card -->
         <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
-          <div class="card border-0 shadow-sm text-center">
+          <div class="card border-1 shadow-sm text-center">
             <img src="/src/assets/images/50061505_575246.png" alt="product.id" />
 
-            <div class="content">
+            <div class="card-body">
               <h5 class="my-5">Typesetting industry</h5>
               <div class="rating">
                 <i class="fa-solid fa-star" style="color: #ffc600"></i>
@@ -61,14 +69,18 @@
                 <span class="last-price">$499</span> <span class="old-price">$599</span>
               </div>
             </div>
+            <div class="overlay-buttons">
+              <button class="add-to-favorite"><i class="fa-regular fa-heart"></i></button>
+              <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"> </i></button>
+            </div>
           </div>
         </div>
         <!-- product card -->
         <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
-          <div class="card border-0 shadow-sm text-center">
+          <div class="card border-1 shadow-sm text-center">
             <img src="/src/assets/images/50061505_575246.png" alt="product.id" />
 
-            <div class="content">
+            <div class="card-body">
               <h5 class="my-5">Typesetting industry</h5>
               <div class="rating">
                 <i class="fa-solid fa-star" style="color: #ffc600"></i>
@@ -81,14 +93,18 @@
                 <span class="last-price">$499</span> <span class="old-price">$599</span>
               </div>
             </div>
+            <div class="overlay-buttons">
+              <button class="add-to-favorite"><i class="fa-regular fa-heart"></i></button>
+              <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"> </i></button>
+            </div>
           </div>
         </div>
         <!-- product card -->
         <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
-          <div class="card border-0 shadow-sm text-center">
+          <div class="card border-1 shadow-sm text-center">
             <img src="/src/assets/images/50061505_575246.png" alt="product.id" />
 
-            <div class="content">
+            <div class="card-body">
               <h5 class="my-5">Typesetting industry</h5>
               <div class="rating">
                 <i class="fa-solid fa-star" style="color: #ffc600"></i>
@@ -101,14 +117,18 @@
                 <span class="last-price">$499</span> <span class="old-price">$599</span>
               </div>
             </div>
+            <div class="overlay-buttons">
+              <button class="add-to-favorite"><i class="fa-regular fa-heart"></i></button>
+              <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"> </i></button>
+            </div>
           </div>
         </div>
         <!-- product card -->
         <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
-          <div class="card border-0 shadow-sm text-center">
+          <div class="card border-1 shadow-sm text-center">
             <img src="/src/assets/images/50061505_575246.png" alt="product.id" />
 
-            <div class="content">
+            <div class="card-body">
               <h5 class="my-5">Typesetting industry</h5>
               <div class="rating">
                 <i class="fa-solid fa-star" style="color: #ffc600"></i>
@@ -121,14 +141,18 @@
                 <span class="last-price">$499</span> <span class="old-price">$599</span>
               </div>
             </div>
+            <div class="overlay-buttons">
+              <button class="add-to-favorite"><i class="fa-regular fa-heart"></i></button>
+              <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"> </i></button>
+            </div>
           </div>
         </div>
         <!-- product card -->
         <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
-          <div class="card border-0 shadow-sm text-center">
+          <div class="card border-1 shadow-sm text-center">
             <img src="/src/assets/images/50061505_575246.png" alt="product.id" />
 
-            <div class="content">
+            <div class="card-body">
               <h5 class="my-5">Typesetting industry</h5>
               <div class="rating">
                 <i class="fa-solid fa-star" style="color: #ffc600"></i>
@@ -141,14 +165,18 @@
                 <span class="last-price">$499</span> <span class="old-price">$599</span>
               </div>
             </div>
+            <div class="overlay-buttons">
+              <button class="add-to-favorite"><i class="fa-regular fa-heart"></i></button>
+              <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"> </i></button>
+            </div>
           </div>
         </div>
         <!-- product card -->
         <div class="col-sm-6 col-md-4 col-lg-3 mt-3">
-          <div class="card border-0 shadow-sm text-center">
+          <div class="card border-1 shadow-sm text-center">
             <img src="/src/assets/images/50061505_575246.png" alt="product.id" />
 
-            <div class="content">
+            <div class="card-body">
               <h5 class="my-5">Typesetting industry</h5>
               <div class="rating">
                 <i class="fa-solid fa-star" style="color: #ffc600"></i>
@@ -160,6 +188,10 @@
               <div class="price">
                 <span class="last-price">$499</span> <span class="old-price">$599</span>
               </div>
+            </div>
+            <div class="overlay-buttons">
+              <button class="add-to-favorite"><i class="fa-regular fa-heart"></i></button>
+              <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"> </i></button>
             </div>
           </div>
         </div>
@@ -172,15 +204,46 @@
 import navBar from '../navBar.vue'
 </script>
 <style lang="scss" scoped>
-.price {
-  margin-top: 15px;
-  margin-bottom: 30px;
-}
+.card {
+  &:hover .overlay-buttons {
+    opacity: 1;
+  }
+  .card-body {
+    position: relative;
+  }
+  .overlay-buttons {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: 0;
+    background: rgba(214, 210, 210, 0.6);
+    transition: opacity 0.5s;
 
-span.last-price {
-  color: #ff4858;
-}
-span.old-price {
-  text-decoration: line-through;
+    .add-to-favorite,
+    .add-to-cart {
+      border-radius: 50%;
+      padding: 10px;
+      margin: 6px;
+      color: rgb(51, 160, 255);
+      border-color: rgba(151, 151, 151, 0.6);
+      margin-bottom: 200px;
+    }
+  }
+  .price {
+    margin-top: 15px;
+    margin-bottom: 30px;
+  }
+
+  span.last-price {
+    color: #ff4858;
+  }
+  span.old-price {
+    text-decoration: line-through;
+  }
 }
 </style>
